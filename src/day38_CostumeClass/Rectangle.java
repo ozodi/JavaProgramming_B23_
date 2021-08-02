993ff3c711ceec14d52b5d7ecdb4853a082ec585
Ozodi Rumiev <rumiev0828@gmail.com>
2021-08-02 09:53:57 -0500

    day38

---
+++ src/day38_CostumeClass/Rectangle.java
@@ -0,0 +1,26 @@
+package day38_CostumeClass;
+
+public class Rectangle {
+
+    public double length, width;
+
+    public void setInfo(double length,double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea(){
+        return length * width;
+    }
+
+    public double calculatePerimeter(){
+        return (length + width) * 2;
+    }
+    public void getInfo(){
+        System.out.println("width = " + width);
+        System.out.println("length = " + length);
+        System.out.println("Area  = "+calculateArea());
+        System.out.println("Perimeter = "+ calculatePerimeter());
+    }
+
+}
