ddd153593d0c72a384b83b4110c1437d9d6eeaa6
Ozodi Rumiev <rumiev0828@gmail.com>
2021-08-03 10:41:14 -0500

    day39

--- src/day39_CustomClass_Statics/carpetTask/Carpet.java
+++ src/day39_CustomClass_Statics/carpetTask/Carpet.java
@@ -5,6 +5,12 @@ public class Carpet {
     public double width, length, unitPrice;
     public boolean isPersian;
 
+    public void customOrder(double width,double length, double unitPrice, boolean isPersian){
+        this.width = width;
+        this.length = length;
+        this.unitPrice = unitPrice;
+        this.isPersian = isPersian;
+    }
 
 
 }
