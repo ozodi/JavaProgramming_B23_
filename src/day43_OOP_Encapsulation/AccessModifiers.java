0c86453824522c5b51b111c778b684d4dbb884f6
Ozodi Rumiev <rumiev0828@gmail.com>
2021-09-30 10:15:58 -0400

    everything

---
+++ src/day43_OOP_Encapsulation/AccessModifiers.java
@@ -0,0 +1,11 @@
+package day43_OOP_Encapsulation;
+
+public class AccessModifiers {
+
+
+    public static int publicVariable = 100;
+    protected static int protectedVariable = 1000;
+    private static int privateVariable = 200;
+
+
+}
